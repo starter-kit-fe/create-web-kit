b53576592e512e06102ccbcd9686f6ff3d215b8f
zh <zh@zhmini-2020.local>
2025-07-28 23:00:52 +0800

    bump version to v25.0728.2300

--- src/templates/nextjs-csr/next.config.ts
+++ src/templates/nextjs-csr/next.config.ts
@@ -1,28 +1,28 @@
-import { format } from 'date-fns';
-
-const nextConfig = {};
+import { format } from "date-fns";
+import { NextConfig } from "next";
+const nextConfig: NextConfig = {};
 
 const proxy = async () => {
-    return [
-        {
-            source: '/api/:path*',
-            destination: 'http://localhost:8000/api/:path*',
-        },
-    ];
+  return [
+    {
+      source: "/api/:path*",
+      destination: "http://localhost:8000/api/:path*",
+    },
+  ];
 };
 
 switch (process.env.NODE_ENV) {
-    case 'production':
-        nextConfig.output = 'export';
-        nextConfig.images = {};
-        nextConfig.images.unoptimized = true;
-        nextConfig.distDir = 'dist';
-        break;
-    case 'development':
-        nextConfig.rewrites = proxy;
-        break;
+  case "production":
+    nextConfig.output = "export";
+    nextConfig.images = {};
+    nextConfig.images.unoptimized = true;
+    nextConfig.distDir = "dist";
+    break;
+  case "development":
+    nextConfig.rewrites = proxy;
+    break;
 }
 
-process.env.NEXT_PUBLIC_BUILD_TIME = format(new Date(), 'yyyy-MM-dd HH:mm');
+process.env.NEXT_PUBLIC_BUILD_TIME = format(new Date(), "yyyy-MM-dd HH:mm");
 
 export default nextConfig;
