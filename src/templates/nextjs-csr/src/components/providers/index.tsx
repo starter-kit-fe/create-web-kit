22b43b395ab0b26523a557c670b53cc31479d4c3
i <i@h06i.com>
2025-11-17 19:26:37 +0800

    bump version to v25.1117.1926

--- src/templates/nextjs-csr/src/components/providers/index.tsx
+++ src/templates/nextjs-csr/src/components/providers/index.tsx
@@ -1,13 +1,16 @@
-import { ThemeProvider } from "./theme";
-import { QueryProvider } from "./query";
-import { Toaster } from "sonner";
+import BuildInfo from "@/components/build-info";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "sonner";
+
+import { QueryProvider } from "./query";
+import { ThemeProvider } from "./theme";
 
 export function Providers({ children }: React.PropsWithChildren) {
   return (
     <QueryProvider>
       <ThemeProvider>
         <TooltipProvider>{children}</TooltipProvider>
+        <BuildInfo />
         <Toaster position="top-right" richColors />
       </ThemeProvider>
     </QueryProvider>
